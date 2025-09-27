20d3fad00d0f8d61933214d5055e1b59a2c6b323
jcor33 <jacobcornelius6@yahoo.com>
2025-09-26 23:58:00 -0400

    Added sidebar ui

---
+++ src/components/Timeline.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+
+export default function Timeline() {
+  return (
+    <div className="bg-white rounded-lg shadow p-8">
+      <div className="text-center text-gray-500">
+        <p>Timeline component - under development</p>
+      </div>
+    </div>
+  );
+}
